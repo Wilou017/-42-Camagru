0bf0c21c5b0df432a3929e7167466e47534881cd
Aymeric MAITRE <amaitre@e2r1p7.42.fr>
2017-02-04 17:00:29 +0100

    init

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/inc/global.php';
-define('PAGENAME', 'Accueil');
+	include_once $_SERVER['DOCUMENT_ROOT'].'/inc/global.php';
+	define('PAGENAME', 'Accueil');
 ?>
 <!DOCTYPE html>
 <html>
